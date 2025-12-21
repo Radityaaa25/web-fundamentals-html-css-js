7c5aa1772c37f9bf19676ee8e39870e64ac211f1
Raditya <radityaanandasatria@gmail.com>
2025-12-21 13:04:16 +0700

    Pomodoro App Done

--- 02-pomodoro/js/script.js
+++ 02-pomodoro/js/script.js
@@ -1,63 +1,78 @@
-const minutesE1 = document.getElementById("minutes");
-const secondsE1 = document.getElementById("seconds");
+const WORK_TIME = 25 * 60;
+const BREAK_TIME = 5 * 60;
 
+let totalSeconds = WORK_TIME;
+let timerInterval = null;
+let isRunning = false;
+
+const minutesDisplay = document.getElementById("minutes");
+const secondsDisplay = document.getElementById("seconds");
 const startBtn = document.getElementById("start-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const resetBtn = document.getElementById("reset-btn");
-
 const workBtn = document.getElementById("work-btn");
 const breakBtn = document.getElementById("break-btn");
 
-let totalSeconds = 25 * 60;
-let timeInterval = null;
-
-const WORK_TIME = 25 * 60;
-const BREAK_TIME = 5 * 60;
+const alarmSound = document.getElementById("alarm-sound")
 
 function updateDisplay() {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
-  
-  minutesE1.textContent = minutes;
-  secondsE1.textContent = seconds < 10 ? "0" + seconds: seconds;
+
+  minutesDisplay.textContent = minutes.toString().padStart(2, "0");
+  secondsDisplay.textContent = seconds.toString().padStart(2, "0");
 }
 
-function startTimer(){
-  if(timeInterval !== null) return;
+function startTimer() {
+  if (timerInterval !== null) return;
+  
+  isRunning = true;
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
 
-  timeInterval = setInterval(() => {
-    if (totalSeconds <= 0) {
-      clearInterval(timeInterval);
-      timeInterval = null;
-      return;
+  timerInterval = setInterval(() => {
+    if (totalSeconds > 0) {
+      totalSeconds--;
+      updateDisplay();
+    } else {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      isRunning = false;
+      
+      alarmSound.play();
+      
+      setTimeout(() => {
+        alert("Waktu Habis!! Istirahat atau mulai kerja lagi.");
+      }, 5000);
+
+      resetTimer();
     }
-
-    totalSeconds--;
-    updateDisplay();
   }, 1000);
 }
 
 function pauseTimer() {
-  clearInterval(timeInterval);
-  timeInterval = null;
+  clearInterval(timerInterval);
+  timerInterval = null;
+  isRunning = false;
+
+  pauseBtn.style.display = "none";
+  startBtn.style.display = "inline-block";
 }
 
 function resetTimer() {
   pauseTimer();
-  totalSeconds = 25 * 60;
+
+  if (workBtn.classList.contains('active')) {
+    totalSeconds = WORK_TIME;
+  } else {
+    totalSeconds = BREAK_TIME;
+  }
+
   updateDisplay();
 }
 
-startBtn.addEventListener("click", startTimer);
-pauseBtn.addEventListener("click", pauseTimer);
-resetBtn.addEventListener("click", resetTimer);
-
-updateDisplay();
-
 function setWorkMode() {
-  clearInterval(timeInterval);
-  timeInterval = null;
-
+  pauseTimer();
   totalSeconds = WORK_TIME;
   updateDisplay();
 
@@ -65,5 +80,21 @@ function setWorkMode() {
   breakBtn.classList.remove("active");
 }
 
+function setBreakMode() {
+  pauseTimer();
+  totalSeconds = BREAK_TIME;
+  updateDisplay();
+
+  breakBtn.classList.add("active");
+  workBtn.classList.remove("active");
+}
+
+updateDisplay();
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
+
 workBtn.addEventListener("click", setWorkMode);
-breakBtn.addEventListener("click", setWorkMode);
+
+breakBtn.addEventListener("click", setBreakMode);
